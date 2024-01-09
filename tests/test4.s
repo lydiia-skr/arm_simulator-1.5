f1437796585a8edcf6d03433244b281ea94284b5
Lydia <111612841+lydiia-skr@users.noreply.github.com>
2024-01-09 10:23:02 +0100

    Update test4.s

--- tests/test4.s
+++ tests/test4.s
@@ -1,15 +1,33 @@
+ .equ MODE_USR, 0x10 @ mode utilisateur 
+.equ MODE_FIQ, 0x11 @ mode FIQ
+.equ MODE_SVC, 0x13 @ mode SVC
+
 .global main
-.text
 
-main:
-    mov r0, #2
-    mov r1, #10   
-    sub r4, r0, r1    
-    mrs r0, cpsr      
 
-    mov r5,#7
-    msr   cpsr, r5
+main:          
 
-    msr   cpsr, #0x10
+ MSR CPSR_f,#0xF0000000
+    MRS R0,CPSR
+    BIC R5,R0,#0x1F
+    ORR R5,R5,#MODE_SVC
+    cmp r0, r5 @ vérifier mode utilisateur
+
+
+    BIC R5,R5,#0x1F
+    ORR R5,R5,#MODE_FIQ
+    msr cpsr_c, R5
+
+    mrs r1, cpsr
+    cmp r1, R5
 
     swi 0x123456
+ 
+end:
+    /* SYS_EXIT */
+    mov r0, #0x18
+    /* ADP_Stopped_ApplicationExit */
+    ldr r1, =#0x20026
+    /* Do the semihosting call on A32. */
+    svc 0x00123456
+
